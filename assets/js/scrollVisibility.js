157d32071c86999d85df5b0f8e7557cd989b2113
Simon Stix <simon.stix@gmail.com>
2022-10-27 15:28:16 +0200

    Fix elements not fading in if they are larger than the screen.

--- assets/js/scrollVisibility.js
+++ assets/js/scrollVisibility.js
@@ -1,13 +1,17 @@
 const intersectionObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(entry.target);
+                let isLargerThanScreen = entry.rootBounds.height < entry.boundingClientRect.height;
+                let isFullyOnSreen = entry.intersectionRatio === 1;
+                if (isLargerThanScreen || isFullyOnSreen) {
+                    entry.target.classList.add("visible");
+                    observer.unobserve(entry.target);
+                }
             }
         })
     },
     {
-        threshold: 1,
+        threshold: [0, 1]
     });
 document.querySelectorAll(".appear").forEach((elem) => {
     intersectionObserver.observe(elem);
